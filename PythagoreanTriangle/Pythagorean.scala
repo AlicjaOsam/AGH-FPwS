a516ea849722c3961b4446a2dd63124b26b5a736
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-25 02:32:30 +0200

    Update Pythagorean.scala

--- PythagoreanTriangle/Pythagorean.scala
+++ PythagoreanTriangle/Pythagorean.scala
@@ -6,7 +6,7 @@ object Pythagorean {
       sqrt(pow(x, 2) - pow(y, 2))
     }else if(n == "c"){
       sqrt(pow(y, 2) - pow(x, 2))
-    } else sqrt(pow(x, 2) + pow(y, 2))
+    }else sqrt(pow(x, 2) + pow(y, 2))
   }
   def main(args: Array[String]): Unit ={
     val third = triangle(args(0).toInt, args(1), args(2).toInt, args(3))
