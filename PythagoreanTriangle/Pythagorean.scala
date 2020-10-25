9d4195a7870f325fd743a11f6410f438af252b51
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-10-25 02:31:55 +0200

    Add files via upload

---
+++ PythagoreanTriangle/Pythagorean.scala
@@ -0,0 +1,15 @@
+import scala.math.{pow, sqrt}
+
+object Pythagorean {
+  def triangle(x: Int, m: String, y: Int, n: String):Double = {
+    if(m == "c"){
+      sqrt(pow(x, 2) - pow(y, 2))
+    }else if(n == "c"){
+      sqrt(pow(y, 2) - pow(x, 2))
+    } else sqrt(pow(x, 2) + pow(y, 2))
+  }
+  def main(args: Array[String]): Unit ={
+    val third = triangle(args(0).toInt, args(1), args(2).toInt, args(3))
+    println(third)
+  }
+}
