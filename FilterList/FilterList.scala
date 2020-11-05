b6817afca6a2a8b3b221da01c0246e9f46863964
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-11-05 14:35:43 +0100

    Update FilterList.scala

--- FilterList/FilterList.scala
+++ FilterList/FilterList.scala
@@ -1,34 +1,19 @@
-import scala.annotation.tailrec
-
-object FilterList {
-  var filtered: List[String] = Nil
-  def filterList(list: List[String], predict: String => Boolean): List[String] ={
-    var filtered: List[String] = Nil
-    @tailrec def check(l: List[String]): Any = {
-      if(l.isEmpty) { return }
-      if(predict(l.head)){
-        filtered = filtered :+ l.head
-      }
-      check(l.tail)
-    }
-    check(list)
-    filtered
+object filterList extends App{
+  @scala.annotation.tailrec
+  def filterList(list: List[String], predicate: String => Boolean, filteredList: List[String] = List()): List[String] = {
+    if(list.isEmpty) return filteredList
+    val l = if(predicate(list.head) )  list.head :: filteredList  else filteredList
+    filterList(list.tail, predicate, l)
   }
 
-  def reverseList(list: List[String]): List[String] ={
-    var reversed: List[String] = Nil
-    @tailrec def reverse(l: List[String]): Any = {
-      if(l.isEmpty) { return }
-      reversed = l.head :: reversed
-      reverse(l.tail)
-    }
-    reverse(list)
-    reversed
+  @scala.annotation.tailrec
+  def reverseList(list: List[String], reversedList: List[String] = List()): List[String] = {
+    if(list.isEmpty) return reversedList
+    val l = list.head :: reversedList
+    reverseList(list.tail, l)
   }
-
-  def main(args: Array[String]): Unit ={
-    val p = (x: String) => x.contains('p')
-    println(filterList(List("Hello", "three", "people"), p))
-    println(reverseList(list = List("Hello", "three", "people")))
-  }
+  
+  val p = (x: String) => x.contains('p')
+  println(filterList(List("Hello", "three", "people"), p))
+  println(reverseList(list = List("Hello", "three", "people")))
 }
