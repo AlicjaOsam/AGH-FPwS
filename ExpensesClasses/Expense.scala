5beebcaa2a4215723aeace873e43a3b894258faf
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-11-07 16:42:16 +0100

    Update Expense.scala

--- ExpensesClasses/Expense.scala
+++ ExpensesClasses/Expense.scala
@@ -1,7 +1,10 @@
+class Expense(val amount: Double, val item_name: String, val category: String) {
+  override def toString: String = {
+    amount + " " + item_name + " " + category
 
-class Expense(val amount: Double, val item_name: String, val category: String) { }
+  }
+}
 
 object Expense{
   def apply(amount: Double, item_name: String, category: String) = new Expense(amount, item_name, category)
 }
-
