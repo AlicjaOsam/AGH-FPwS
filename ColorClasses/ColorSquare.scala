fb10eb65a606a997a25690519ab5395222b305ab
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-11-07 15:26:36 +0100

    Delete ColorSquare.scala

--- ColorClasses/ColorSquare.scala
+++
@@ -1,8 +0,0 @@
-class ColorSquare(center: (Int,Int), size: Int, var color: (Int,Int,Int))
-  extends Square(center,size) with HasColor {
-  override def toString: String = s"Square of size $size at $center with color r:$red g:$green b: $blue"
-}
-
-object ColorSquare {
-  def apply(center:(Int,Int), size:Int, color:(Int,Int,Int)) = new ColorSquare(center, size, color)
-}
