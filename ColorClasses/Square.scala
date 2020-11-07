98a609137862d98ccf07a30a4d3b3c160490f4a4
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2020-11-07 16:37:11 +0100

    Delete Square.scala

--- ColorClasses/Square.scala
+++
@@ -1,7 +0,0 @@
-class Square(center: (Int,Int), size: Int) extends Shape(center, size) {
-  override def toString: String = s"Square of size $size at $center"
-}
-
-object Square{
-  def apply(center:(Int,Int), size:Int) = new Square(center,size)
-}
